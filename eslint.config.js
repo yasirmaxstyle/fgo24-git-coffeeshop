9a3c028ed02a932dd1ef4fbb716693e3dd1395f2
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-26 03:50:09 +0700

    chore: add eslint rules for indentation and semicolon use

--- eslint.config.js
+++ eslint.config.js
@@ -6,4 +6,10 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+    }
+  }
 ]);
