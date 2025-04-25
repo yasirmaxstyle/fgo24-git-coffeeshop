8281b664db1fa4630e6b1cd05218e9098afea8e8
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-26 04:20:45 +0700

    chore: add eslint plugin stylistics and some rules of array bracket

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   {
+    plugins: { "@stylistic/js": stylisticJs },
     rules: {
       "semi": ["error", "always"],
       "indent": ["error", 2],
+      "@stylistic/js/array-bracket-newline": ["error", "always"]
+
     }
   }
 ]);
